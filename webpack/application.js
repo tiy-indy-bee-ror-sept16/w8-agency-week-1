849731dd9830e68c7bf439ce976e6ad639ae7c93
Sally Wieland <sallybwieland@gmail.com>
2016-11-15 11:59:57 -0500

    updated package.json with react/babel files, updated html page with bootstrap and meta-tags

--- webpack/application.js
+++ webpack/application.js
@@ -19,20 +19,21 @@ import Checkout from '../components/Checkout'
 // window.path = window.location.href.includes('github') ? '/w6d4-make-chirp' : ''
 // makes global var called path
 
-window.renderView = function() { // need to call because when we get a response back, we need to recall render so it does another API call to actually show the photo on screen
-    ReactDOM.render(
-    <Router history ={browserHistory}>
-        {/* <Route path="/" component={Home} /> */}
-        <Route path={path + '/'} component={Home} />
-        <Route path={path + '/music'} component={Music} />
-        <Route path={path +  '/farming'} component={Farming} />
-        <Route path={path + '/nationalparks'} component={NationalParks} />
-        <Route path={path + '/movies'} component={Movies} />
-        <Route path={path + '/searchresults'} component={SearchResults} />
-        <Route path={path + '/checkout'} component={Checkout} />
-    </Router> ,
-    document.querySelector('#app')
+document.addEventListener('DOMContentLoaded', function(event) {
+    ReactDOM.render( // talk with back-end about 
+        <Router history ={browserHistory}>
+            {/* <Route path="/" component={Home} /> */}
+            <Route path={path + '/'} component={Home} />
+            <Route path={path + '/music'} component={Music} />
+            <Route path={path +  '/farming'} component={Farming} />
+            <Route path={path + '/nationalparks'} component={NationalParks} />
+            <Route path={path + '/movies'} component={Movies} />
+            <Route path={path + '/searchresults'} component={SearchResults} />
+            <Route path={path + '/checkout'} component={Checkout} />
+        </Router> ,
+        document.querySelector('#app')
     )
-}
+})
+
 
 renderView()
