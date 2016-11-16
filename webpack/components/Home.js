d1a66830ee946cec377dd3a11d08dda3c2fb1db4
Brian McMillan <brianm_91@hotmail.com>
2016-11-16 16:08:14 -0500

    Updated application.html.erb, added feature component, added link to home.js

--- webpack/components/Home.js
+++ webpack/components/Home.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './Header'
+import { Link } from 'react-router'
 
 class Home extends React.Component {
     constructor(props) {
         super(props)
     }
     render() {
-        return (
-            // <h1>hello</h1>
+        return <div>
             <Header />
-        )
+            <Link to='/items'>
+            <button type='button' className='btn btn-default'>Click</button>
+            </Link>
+        </div>
     }
 }
 
