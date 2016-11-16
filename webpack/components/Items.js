7a771ad2d949606d83a6cab268d7f9c347a49dc4
kwhuff <kwhuff@gmail.com>
2016-11-16 13:50:33 -0500

    worked on item.js and items.js

--- webpack/components/Items.js
+++ webpack/components/Items.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import Item from './Item'
 
 class Items extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+          allPatches: [
+              {
+              picture: 'https://unsplash.it/600?random',
+              product: 'This is product name',
+              price: '$55.00'
+                },
+            {
+            picture: 'https://unsplash.it/600?random',
+            product: 'another product',
+            price: '$44.00'
+              }
+
+        ]
+        }
     }
+    // fetchAllPatches(){
+    //     fetch(OURAPICALLGOESHERE)
+    //     .then(response => response.json())
+    //     .then(response => this.setState({allPatches: response}))
+    // }
     render() {
-        return <h1>hello</h1>
+        var items = this.state.allPatches.map((data, i) => {
+      return <Item data={data} key={i} />
+        })
+        return <div>
+                <div className="container">
+                    <div className="row">
+                        {items}
+                    </div>
+                </div>
+        </div>
     }
 }
 
