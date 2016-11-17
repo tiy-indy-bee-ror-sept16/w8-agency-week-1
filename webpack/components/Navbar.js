68919fa881fe0bc8221169af28181d9360ee9200
Sally Wieland <sallybwieland@gmail.com>
2016-11-17 17:07:31 -0500

    minor changes

--- webpack/components/Navbar.js
+++ webpack/components/Navbar.js
@@ -11,19 +11,19 @@ render() {
       return <div>
             <div className='tabLinks text-center'>
               <Link to='/Music'>
-              <button className='link1'>Music</button>
+                <button className='link1'>Music</button>
               </Link>
               <Link to='/Movies'>
-              <button className='link2'>Movies</button>
+                <button className='link2'>Movies</button>
               </Link>
               <Link to='/NationalParks'>
-              <button className='link3'>National Parks</button>
+                <button className='link3'>National Parks</button>
               </Link>
               <Link to='/farming'>
-              <button className='link4'>Farming</button>
+                <button className='link4'>Farming</button>
               </Link>
               <Link to='/Items'>
-              <button className='link5'>All Patches</button>
+                <button className='link5'>All Patches</button>
               </Link>
             </div>
   </div>
