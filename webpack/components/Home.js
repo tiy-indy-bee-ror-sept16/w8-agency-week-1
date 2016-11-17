7cddbeecb5b6dc20191f14652e48b9e8d9b90151
Sally Wieland <sallybwieland@gmail.com>
2016-11-17 17:58:34 -0500

    changed up modal stuff

--- webpack/components/Home.js
+++ webpack/components/Home.js
@@ -10,11 +10,17 @@ import FarmingHome from './FarmingHome'
 import MovieHome from './MovieHome'
 import NationalParksHome from './NationalParksHome'
 import Footer from './Footer'
+import Item from './Item'
+import Modal from 'react-modal'
+import ModalComp from './ModalComp'
 
 class Home extends React.Component {
     constructor(props) {
         super(props)
     }
+    componentWillMount() {
+        Modal.setAppElement('body');
+     }
     render() {
         return <div>
         <div className="container-fluid">
@@ -28,7 +34,7 @@ class Home extends React.Component {
         </div>
         <div className="container-fluid containerMargin">
             <div className="row borderBottom">
-                <Feature />
+                <Feature /> <button onClick={this.openModal}>hi</button>
                 <Link to='/items'>
                 <button type='button' className='btn btn-default'>Show All</button>
                 </Link>
@@ -51,6 +57,8 @@ class Home extends React.Component {
             <Footer />
             </div>
         </div>
+
+        <Modal/>
         </div>
     }
 }
--- webpack/components/Item.js
+++ webpack/components/Item.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Modal from 'react-modal'
+import Modal from 'react-modal'
 
-const customStyles = {
-content : {
-top                   : '50%',
-left                  : '50%',
-right                 : 'auto',
-bottom                : 'auto',
-marginRight           : '-50%',
-transform             : 'translate(-50%, -50%)'
-}
-};
+// const customStyles = {
+// content : {
+// top                   : '50%',
+// left                  : '50%',
+// right                 : 'auto',
+// bottom                : 'auto',
+// marginRight           : '-50%',
+// transform             : 'translate(-50%, -50%)'
+// }
+// };
 
 class Item extends React.Component {
     constructor(props) {
