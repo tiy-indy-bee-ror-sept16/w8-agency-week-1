05e10da646c4e1746ce6c19c73c4806b05fe82e9
keilsmit <keilsmit@yahoo.com>
2016-11-17 23:28:07 -0500

    tested stripe gem can pay for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,10 +11,23 @@ class CartsController < ApplicationController
   end
 
 
+  def update
+    @cart = Cart.find_by(token: params[:token])
+    @cart.update!(cart_params)
+      if @cart.save
+        render json: @cart
+      else
+        render json: @cart.errors
+      end
+  end
 
 
 
+private
 
+def cart_params
+  params.permit(:shipping_address, :token)
+end
 
 
 
